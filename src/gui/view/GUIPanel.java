717101c8dc28f8605ca04c87b767a792c7f0b83f
JordanBurnett1210 <jabur1210@gmail.com>
2015-11-09 09:47:46 -0700

    did the things.

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -4,6 +4,7 @@ import javax.swing.JPanel;
 import gui.controller.Controller;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
 /**
  * @author jbur0473
@@ -16,11 +17,13 @@ public class GUIPanel extends JPanel
 	private Controller baseController;
 	private JButton firstButton;
 	private JTextField firstField;
+	private SpringLayout baseLayout;
 	
 	public GUIPanel(Controller baseController)
 	{
 		this.baseController = baseController;
 		
+		baseLayout = new SpringLayout();
 		firstButton = new JButton("Please do not click the button");
 		firstField = new JTextField("words can be typed here");
 		
@@ -29,9 +32,14 @@ public class GUIPanel extends JPanel
 		setupListeners();
 	}
 	
+	/**
+	 * Helper method to load all GUI components into the panel.
+	 */	
 	private void setupPanel()
 	{
-		
+		this.setLayout(baseLayout);
+		this.add(firstButton);
+		this.add(firstField);
 	}
 	
 	private void setupLayout()
